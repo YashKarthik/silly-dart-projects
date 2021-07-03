0441cf2b01dee5f62e8a8bc521ae64cbf8655dfe
Yashashwin Karthikeyan <yash_karthik_10@protonmail.com>
2021-07-03 13:53:11 +0530

    initial file

---
+++ tic-tac-toe.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+/*
+1. Print out a 3x3 matrix of dots
+2. Ask the user for a (x,y) coordinate of their mark (O)
+3. Reprint the 3x3 matrix with an update for the user's mark
+4. Reprint the matrix with the robot's mark (x)
+5. Repeat...
+6. When a line of same the same marks is formed, end the game
+*/
+
+void reprint(){
+	print('''
+		.	.	.
+
+		.	.	.
+
+		.	.	.''');
+}
+
+void main(){
+	reprint();
+}
