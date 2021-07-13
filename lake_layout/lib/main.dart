7ae3328482fb6eec14cd8adbe18b02f7d2c09d5b
Yashashwin Karthikeyan <yash_karthik_10@protonmail.com>
2021-07-13 14:25:03 +0530

    done

--- lake_layout/lib/main.dart
+++ lake_layout/lib/main.dart
@@ -1,113 +1,150 @@
 import 'package:flutter/material.dart';
+// TODO: create custom widget for the buttons and text combined
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(home:MyApp()));
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text(
+					'Layout demo',
+					style: TextStyle(
+						color: Colors.white,
+				  ),
+				),
+			),
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+			body: Column(
+				children: <Widget>[
+					Image.asset('assets/lake.jpg'),
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+					SizedBox(height: 30),
 
-  final String title;
+					Row(
+						children: <Widget>[
+							SizedBox(width: 20),
+							Column(
+								crossAxisAlignment: CrossAxisAlignment.start,
+								children: <Widget>[
+									Text(
+										'Oeschinen Lake Campground',
+										style: TextStyle(
+											fontWeight: FontWeight.bold,
+										),
+									),
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+									SizedBox(height: 8),
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+									Text(
+										'Kandersteg, Switzerland',
+										style: TextStyle(
+											fontWeight: FontWeight.w400,
+											color: Colors.grey[600], 
+										),
+									),
+								],
+							),
+							SizedBox(width: 80),
+							Row(
+								children: <Widget>[
+									Icon(
+										Icons.star,
+										color: Colors.orange
+									),
+									Text(
+										'41',
+										style: TextStyle(
+											color: Colors.grey[950],
+										),
+									),
+								],
+							),
+						],
+					),
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+					SizedBox(height: 30),
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+					Column(
+						children: <Widget>[
+						Row(
+							mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+							children: <Widget>[
+							TextButton(
+								onPressed: () {},
+								child: Column(
+									children: <Widget>[
+										Icon(
+											Icons.call,
+											color: Colors.lightBlue,
+										),
+										SizedBox(height: 10,),
+										Text(
+											'CALL',
+											style: TextStyle(
+												color: Colors.lightBlue,
+											),
+										)
+									],
+								),
+							),
+
+							TextButton(
+								onPressed: () {},
+								child: Column(
+									children: <Widget>[
+										Icon(
+											Icons.near_me,
+											color: Colors.lightBlue,
+										),
+										SizedBox(height: 10,),
+										Text(
+											'ROUTE',
+											style: TextStyle(
+												color: Colors.lightBlue,
+											),
+										)
+									],
+								),
+							),
+
+							TextButton(
+								onPressed: () {},
+								child: Column(
+									children: <Widget>[
+										Icon(
+											Icons.share,
+											color: Colors.lightBlue,
+										),
+										SizedBox(height: 10,),
+										Text(
+											'SHARE',
+											style: TextStyle(
+												color: Colors.lightBlue,
+											),
+										)
+									],
+								),
+							),
+							],
+						),
+						SizedBox(height: 10),
+						Row(),
+						],
+					),
+					SizedBox(height: 10),
+
+					Container(
+						child: Text(
+							"Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.,"	
+						),
+
+						padding: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+					),
+				],
+			),
+		);
+	}
 }
