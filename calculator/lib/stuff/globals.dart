5b1077f5c72913f4736141d6ae97bc14c613970f
Yash Karthik <yash_karthik_10@protonmail.com>
2021-08-08 16:03:15 +0530

    delete calculator, will work on it later

--- calculator/lib/stuff/globals.dart
+++
@@ -1,4 +0,0 @@
-library calculator.globals;
-
-int solution = 0;
-String operation = '';
--- calculator/lib/test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:expressions/expressions.dart';
-
-void main(){
-	var exp = Expression.parse('1 + 1');
-	var context = {'x' : 3};
-	final evaluator = const ExpressionEvaluator();
-	print(evaluator.eval(exp, context));
-}
